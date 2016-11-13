287ef19d83ccdb0ac538078fac4396b67f68e06d
jane <849467474@qq.com>
2016-11-13 21:57:47 +0800

    字符串 频度  串树实现

---
+++ fourth_task/bun.cpp
@@ -0,0 +1,248 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+struct addr
+{
+	int place;
+	addr *next;
+};
+struct letter
+{
+	char alphabet;
+	int count;
+	addr *addr_count;
+	letter *sibling;
+	letter *next;
+};
+
+void init_letter(letter *p)
+{
+	p->addr_count=NULL;
+	p->alphabet='A';
+	p->count=0;
+	p->sibling=0;
+	p->next=NULL;
+}
+void init(letter *p)
+{
+	int i=0;
+	for(i=0;i<26;i++)
+	{
+		p[i].alphabet='A'+i;
+		p[i].sibling=NULL;
+		p[i].addr_count=NULL;
+		p[i].next=NULL;
+		p[i].count=0;
+		p[i+26].alphabet='a'+i;
+		p[i+26].sibling=NULL;
+		p[i+26].next=NULL;
+		p[i+26].count=0;
+		p[i+26].addr_count=NULL;
+	}
+}
+letter* set_p(char chr,letter *p,letter *forest)
+{
+	if(chr>='A'&&chr<='Z')
+	{
+		p=&forest[((int)chr-'A')];
+	}else if(chr>='a'&&chr<='z')
+	{
+		p=&forest[((int)chr-'a'+26)];
+	}
+	cout<<"first_al: "<<p->alphabet<<endl;
+	return p;
+}
+letter* insert(char chr,letter *p)
+{
+	letter *q,*cur,*origin;
+	q = new letter;
+	q->addr_count=NULL;
+	q->alphabet=chr;
+	q->sibling=NULL;
+	q->next=NULL;
+	q->count=0;
+	if(p->next==NULL)
+	{
+		p->next=q;
+		return p;
+	}else{
+		origin=p;
+		p=p->next;
+		cur=p;
+			
+		if(p->alphabet>chr)
+		{
+			q->sibling=p;
+			origin->next=q;
+			p=q;
+			return p;
+		}else{
+			while(p!=NULL)
+			{
+				if(p->alphabet<chr)
+				{
+					cur=p;
+					p=p->sibling;
+				}else{
+					break;
+				}
+			}
+			if(p->alphabet==chr)
+			{
+				return p;
+			}
+			else{
+				cur->sibling=q;
+				p=q;
+			}
+			
+		}
+	}
+	return p;
+
+}
+void set_record(char chr,letter *p,int place)
+{
+	addr *q;
+	q = new addr;
+	q->next=NULL;
+	q->place=place;
+	if(p->addr_count)
+		q->next=p->addr_count->next;
+	p->addr_count=q;
+}
+void display(letter *forest)
+{
+	int i=0;
+	letter *q;
+//	q=NULL;
+	cout<<"oooo"<<endl;
+	for(i=0;i<52;i++)
+	{
+		cout<<forest[i].alphabet<<" "<<endl;
+		if(forest[i].next!=NULL)
+		{
+			if(forest[i].next->sibling!=NULL)
+			{
+				q=forest[i].next->sibling;
+				while(q->sibling!=NULL)
+				{
+					cout<<q->alphabet<<"\t";
+					q=q->sibling;
+				}
+			}
+				
+			
+			}		
+		
+
+	}
+}
+void man_txt()
+{
+	fstream f1;
+	letter *p;
+	addr *q;
+		letter *q_l;
+	letter forest[52];
+	letter *origin,*cur;
+	int place=0;
+	char chr;
+	init(forest);
+	f1.open("1.txt",ios::in);
+//	f2.open("en2.txt",ios::out);
+	display(forest);
+	while(!f1.eof())
+	{
+		place=f1.tellg();
+		chr=f1.get();
+		while(!((chr>='A'&&chr<='Z')||(chr>='a'&&chr<='z'))&&!f1.eof())
+		{
+			place=f1.tellg();
+			chr=f1.get();
+			cout<<"o"<<endl;
+		}            
+		if(!f1.eof()&&((chr>='A'&&chr<='Z')||(chr>='a'&&chr<='z')))
+		p=set_p(chr,p,forest);
+	//	cout<<chr<<" "<<"ppp"<<endl;
+		while(!f1.eof()&&((chr>='A'&&chr<='Z')||(chr>='a'&&chr<='z')))
+		{
+			chr=f1.get();
+
+			cout<<"insertint.. : "<<p->alphabet<<endl;
+		//	p=insert(chr,p);
+
+			q_l = new letter;
+			q_l->addr_count=NULL;
+			q_l->alphabet=chr;
+			q_l->sibling=NULL;
+			q_l->next=NULL;
+			q_l->count=0;
+			cout<<"pppp---"<<endl;
+			if(p->next==NULL)
+			{
+				p->next=q_l;
+			//	return p;
+				cout<<"ppp "<<chr<<endl;
+			//	display(forest);
+			}else{
+				origin=p;
+				p=p->next;
+				cur=p;
+				if(p->alphabet>chr)
+				{
+					q_l->sibling=p;
+					origin->next=q_l;
+					p=q_l;
+			//		return p;
+				}else{
+					while(p!=NULL)。
+					{
+						if(p->alphabet<chr)
+						{
+							cur=p;
+							p=p->sibling;
+						}else{
+							break;
+						}
+					}
+					if(p==NULL)
+					{
+						cur->sibling=q_l;
+						p=q_l;
+					}else if(p->alphabet>chr)
+					{
+						q_l->sibling=p;
+						cur->sibling=q_l;
+					}										
+				}
+			}
+		//  p=insert(chr,p);
+		}
+		//set_record(chr,p,place);
+		cout<<"OPOP"<<endl;
+		q = new addr;
+		q->next=NULL;
+		q->place=place;
+		p->count++;
+		if(p->addr_count)
+			q->next=p->addr_count->next;
+		p->addr_count=q;
+		cout<<"p->cout : "<<p->count<<endl;
+		//set_record(chr,p,place);
+
+	}
+	f1.close();
+//	f2.close();
+	display(forest);
+	cout<<"end";
+}
+
+
+
+int main()
+{
+	man_txt();
+	cout<<('A'=='A')<<endl;
+	return 0;
+}
---
+++ fourth_task/bunch.cpp
@@ -0,0 +1,253 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+struct addr
+{
+	int place;
+	addr *next;
+};
+struct letter
+{
+	char alphabet;
+	int count;
+	addr *addr_count;
+	letter *sibling;
+	letter *next;
+};
+
+void init_letter(letter *p)
+{
+	p->addr_count=NULL;
+	p->alphabet='A';
+	p->count=0;
+	p->sibling=0;
+	p->next=NULL;
+}
+void init(letter *p)
+{
+	int i=0;
+	for(i=0;i<26;i++)
+	{
+		p[i].alphabet='A'+i;
+		p[i].sibling=NULL;
+		p[i].addr_count=NULL;
+		p[i].next=NULL;
+		p[i].count=0;
+		p[i+26].alphabet='a'+i;
+		p[i+26].sibling=NULL;
+		p[i+26].next=NULL;
+		p[i+26].count=0;
+		p[i+26].addr_count=NULL;
+	}
+}
+letter* set_p(char chr,letter *p,letter *forest)
+{
+	if(chr>='A'&&chr<='Z')
+	{
+		p=&forest[((int)chr-'A')];
+	}else if(chr>='a'&&chr<='z')
+	{
+		p=&forest[((int)chr-'a'+26)];
+	}
+	cout<<"first_al: "<<p->alphabet<<endl;
+	return p;
+}
+letter* insert(char chr,letter *p)
+{
+	letter *cur,*origin,*q_l;
+	q_l = new letter;
+			q_l->addr_count=NULL;
+			q_l->alphabet=chr;
+			q_l->sibling=NULL;
+			q_l->next=NULL;
+			q_l->count=0;
+			cout<<"pppp---"<<endl;
+			if(p->next==NULL)
+			{
+				p->next=q_l;
+			//	return p;
+				p=q_l;
+				cout<<"ppp "<<chr<<endl;
+			//	display(forest);
+			}else{
+				origin=p;
+				p=p->next;
+				cur=p;
+				if(p->alphabet>chr)
+				{
+					q_l->sibling=p;
+					origin->next=q_l;
+					p=q_l;
+			//		return p;
+				}else{
+					while(p!=NULL)
+					{
+						if(p->alphabet<chr)
+						{
+							cur=p;
+							p=p->sibling;
+						}else{
+							break;
+						}
+					}
+					if(p==NULL)
+					{
+						cur->sibling=q_l;
+						p=q_l;
+					}else if(p->alphabet>chr)
+					{
+						q_l->sibling=p;
+						cur->sibling=q_l;
+					}										
+				}
+			}
+	return p;
+
+}
+void set_record(char chr,letter *p,int place)
+{
+	addr *q;
+	q = new addr;
+	q->next=NULL;
+	q->place=place;
+	if(p->addr_count)
+		q->next=p->addr_count->next;
+	p->addr_count=q;
+}
+void display(letter *forest)
+{
+	int i=0;
+	letter *q_l;
+//	q=NULL;
+	cout<<"oooo"<<endl;
+	for(i=0;i<52;i++)
+	{
+		cout<<forest[i].alphabet<<" ";
+		if(forest[i].next!=NULL)
+		{
+			q_l=forest[i].next;
+				while(q_l->next!=NULL)
+				{
+					cout<<q_l->alphabet<<"\t";
+					q_l=q_l->next;
+				}	
+				cout<<q_l->alphabet<<"\t";
+				cout<<q_l->count;
+		}		
+		cout<<" "<<endl;
+
+	}
+}
+void man_txt()
+{
+	fstream f1;
+	letter *p;
+	addr *q;
+		letter *q_l;
+	letter forest[52];
+	letter *origin,*cur;
+	int place=0;
+	char chr;
+	init(forest);
+	f1.open("1.txt",ios::in);
+//	f2.open("en2.txt",ios::out);
+	display(forest);
+	while(!f1.eof())
+	{
+		place=f1.tellg();
+		chr=f1.get();
+		while(!((chr>='A'&&chr<='Z')||(chr>='a'&&chr<='z'))&&!f1.eof())
+		{
+			place=f1.tellg();
+			chr=f1.get();
+			cout<<"o"<<endl;
+		}            
+		if(!f1.eof()&&((chr>='A'&&chr<='Z')||(chr>='a'&&chr<='z')))
+		p=set_p(chr,p,forest);
+		while(!f1.eof()&&((chr>='A'&&chr<='Z')||(chr>='a'&&chr<='z')))
+		{
+			chr=f1.get();
+
+			cout<<"insertint.. : "<<chr<<" "<<p->alphabet<<endl;
+			if((chr>='A'&&chr<='Z')||(chr>='a'&&chr<='z'))
+			{	
+		//	p=insert(chr,p);
+			
+			q_l = new letter;
+			q_l->addr_count=NULL;
+			q_l->alphabet=chr;
+			q_l->sibling=NULL;
+			q_l->next=NULL;
+			q_l->count=0;
+			cout<<"pppp---"<<endl;
+			if(p->next==NULL)
+			{
+				p->next=q_l;
+			//	return p;
+				p=q_l;
+				cout<<"ppp "<<chr<<endl;
+			//	display(forest);
+			}else{
+				origin=p;
+				p=p->next;
+				cur=p;
+				if(p->alphabet>chr)
+				{
+					q_l->sibling=p;
+					origin->next=q_l;
+					p=q_l;
+			//		return p;
+				}else{
+					while(p!=NULL)
+					{
+						if(p->alphabet<chr)
+						{
+							cur=p;
+							p=p->sibling;
+						}else{
+							break;
+						}
+					}
+					if(p==NULL)
+					{
+						cur->sibling=q_l;
+						p=q_l;
+					}else if(p->alphabet>chr)
+					{
+						q_l->sibling=p;
+						cur->sibling=q_l;
+					}										
+				}
+			}
+		//  p=insert(chr,p);
+		}
+		}
+		//set_record(chr,p,place);
+		cout<<"OPOP"<<endl;
+		q = new addr;
+		q->next=NULL;
+		q->place=place;
+		p->count++;
+		if(p->addr_count)
+			q->next=p->addr_count->next;
+		p->addr_count=q;
+		cout<<"p->cout : "<<p->count<<endl;
+		//set_record(chr,p,place);
+
+
+	}
+	//cout<<forest['a'-'a'+26].next->alphabet<<endl;
+	f1.close();
+//	f2.close();
+	display(forest);
+	cout<<"end";
+}
+
+
+
+int main()
+{
+	man_txt();
+	cout<<('A'=='A')<<endl;
+	return 0;
+}
