ae0842fbee6f8c3fb595bd2fbf89d8548f29e6cc
[Jane] <[849467474@qq.com]>
2017-01-04 14:48:47 +0800

    最佳灌溉OK

--- final_big_work/best_watering/best_watering.cpp
+++ final_big_work/best_watering/best_watering.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<queue>
 #include<stack>
+#define TEST_FILE "all_test.txt"
 #include<fstream>
 using namespace std;
 typedef struct country
@@ -22,20 +23,24 @@ typedef struct all_country
 	country all_c[100];
 	int country_num;
 	int L_num;
-
 }all_country;
 int visit[100];
-void read_table(all_country &C)
+int read_table(char * test_file,all_country &C)
 {
 	fstream f1;
-	f1.open("best_watering.txt",ios::in);
+//	f1.open("best_watering.txt",ios::in);
+	f1.open(test_file,ios::in);
+	if(f1.fail())
+	{
+		return 0;
+	}
 	f1>>C.country_num>>C.L_num;
 	country *p;
 	int from,to,weight;
 	for(int i=0;i<C.L_num;i++)
 	{
 		f1>>from>>to>>weight;
-		cout<<from<<" "<<to<<" "<<weight<<endl;
+	//	cout<<from<<" "<<to<<" "<<weight<<endl;
 		p=new country;
 		p->next_country=to;
 		p->weight=weight;
@@ -49,16 +54,18 @@ void read_table(all_country &C)
 		C.all_c[to].next_sibling=p;
 	}
 	f1.close();
+	return 1;
 }
-void get_mini(all_country C,int node,int &min_node,int &min_weight)
+int get_mini(all_country C,int node,int &min_node,int &min_weight)
 {
 	country *p;
 	min_node=-1;
 	min_weight=-1;
+	int flag=0;
 	p=C.all_c[node].next_sibling;
 	while(p)
 	{
-	//	cout<<endl<<visit[p->next_country]<<endl;
+	//	cout<<endl<<p->next_country<<" "<<visit[p->next_country]<<endl;
 		if(visit[p->next_country]==0)
 		{
 			if(min_weight==-1||(p->weight<min_weight))
@@ -67,9 +74,11 @@ void get_mini(all_country C,int node,int &min_node,int &min_weight)
 				min_node=p->next_country;
 			}
 	//		cout<<endl<<"node: "<<node<<" min_node: "<<min_node<<" "<<min_weight<<endl;
+			flag=1;
 		}
 		p=p->next_sibling;
 	}
+	return flag;
 }
 void mini_road(all_country C)
 {
@@ -78,17 +87,14 @@ void mini_road(all_country C)
 	int n=C.country_num;
 	queue<Save_node> q;
 	stack<int> p;
-	int min_node;
-	int min_weight;
+	int min_node=-1;
+	int min_weight=-1;
 	p.push(v);
 	visit[v]=1;
 	n--;
 	while(n)
 	{
-		get_mini(C,v,min_node,min_weight);
-
-	//	cout<<min_node<<" "<<min_weight<<endl;
-	//	system("pause");
+	
 		if(min_node==-1)
 		{
 			v=p.top();
@@ -98,10 +104,13 @@ void mini_road(all_country C)
 		{
 			q.push(Save_node(v,min_node,min_weight));
 			visit[min_node]=1;
-			p.push(min_node);
+			p.push(v);
 			v=min_node;
 			n--;
 		}
+		get_mini(C,v,min_node,min_weight);
+	//	cout<<v<<" "<<min_node<<" "<<min_weight<<" "<<n<<endl;
+	//	system("pause");
 	}
 	Save_node l;
 	int sum=0;
@@ -133,12 +142,32 @@ void display_table(all_country C)
 }
 int main()
 {
+	fstream f_test;
+	int i=1;
+	char test_file_name[30];
+	f_test.open("all_test.txt",ios::in);
+	if(f_test.fail())
+	{
+		cout<<"can't open file: "<<TEST_FILE<<endl;
+		exit(1);
+	}
+	while(!f_test.eof())
+	{
+		f_test>>test_file_name;
+		cout<<"test_file "<<i<<": "<<test_file_name<<endl<<endl;
+		all_country C;
+		if(read_table(test_file_name,C))
+		{
+		//	display_table(C);
+			mini_road(C);
+			cout<<endl;
+		}
+		else
+		{
+			cout<<"can't open file: "<<test_file_name<<endl;
+		}
+	
+	}
 
-	all_country C;
-	read_table(C);
-	cout<<endl;
-	display_table(C);
-	cout<<endl;
-	mini_road(C);
 	return 0;
 }
